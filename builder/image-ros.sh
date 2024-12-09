2e1e2b6a5c775c59fa8c32757df8278405b83309
jhinmainksta <gtslnok@bk.ru>
2024-12-10 09:06:36 +1100

    Could not find a package configuration file provided by "tf2_geometry_msgs"

--- builder/image-ros.sh
+++ builder/image-ros.sh
@@ -103,7 +103,8 @@ ros-${ROS_DISTRO}-web-video-server
 my_travis_retry apt-get install -y --no-install-recommends \
     ros-${ROS_DISTRO}-led-msgs \
     ros-${ROS_DISTRO}-tf \
-    ros-${ROS_DISTRO}-tf2
+    ros-${ROS_DISTRO}-tf2 \
+    ros-${ROS_DISTRO}-tf2_geometry_msgs
 
 echo_stamp "Installing libboost-dev" # https://travis-ci.org/github/CopterExpress/clover/jobs/766318908#L6536
 my_travis_retry apt-get install -y --no-install-recommends libboost-dev libboost-all-dev
